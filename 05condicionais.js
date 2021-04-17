687c2139e01ae332f5d0c6cf42f77527647eb218
cadumega <72901445+cadumega@users.noreply.github.com>
2021-04-17 01:50:32 -0300

    condicionais_part3

---
+++ 05condicionais.js
@@ -0,0 +1,239 @@
+/* 
+*)Introducing Else Statements
+When a condition for an if statement is true, the block of code following it is executed. What about when that condition is false? Normally nothing would happen. With an else statement, an alternate block of code can be executed.
+
+if (num > 10) {
+  return "Bigger than 10";
+} else {
+  return "10 or Less";
+}
+
+ex) Combine the if statements into a single if/else statement.
+
+r)function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+*)Introducing Else If Statements
+If you have multiple conditions that need to be addressed, you can chain if statements together with else if statements.
+
+if (num > 15) {
+  return "Bigger than 15";
+} else if (num < 5) {
+  return "Smaller than 5";
+} else {
+  return "Between 5 and 15";
+}
+
+ex) Convert the logic to use else if statements.
+
+r)function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+  return "Between 5 and 10";
+}
+}
+
+testElseIf(7);
+
+*)Logical Order in If Else Statements
+Order is important in if, else if statements.
+
+The function is executed from top to bottom so you will want to be careful of what statement comes first.
+
+Take these two functions as an example.
+
+Here's the first:
+
+function foo(x) {
+  if (x < 1) {
+    return "Less than one";
+  } else if (x < 2) {
+    return "Less than two";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+And the second just switches the order of the statements:
+
+function bar(x) {
+  if (x < 2) {
+    return "Less than two";
+  } else if (x < 1) {
+    return "Less than one";
+  } else {
+    return "Greater than or equal to two";
+  }
+}
+While these two functions look nearly identical if we pass a number to both we get different outputs.
+
+foo(0)
+bar(0)
+
+//foo(0) will return the string Less than one, 
+//and bar(0) will return the string Less than two.
+
+ex) Change the order of logic in the function so that it will return the correct statements in all cases.
+
+r)
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+
+*)Chaining If Else Statements
+if/else statements can be chained together for complex logic. Here is pseudocode of multiple chained if / else if statements:
+
+if (condition1) {
+  statement1
+} else if (condition2) {
+  statement2
+} else if (condition3) {
+  statement3
+. . .
+} else {
+  statementN
+}
+
+ex) Write chained if/else if statements to fulfill the following conditions:
+
+num < 5 - return Tiny
+num < 10 - return Small
+num < 15 - return Medium
+num < 20 - return Large
+num >= 20 - return Huge
+
+r)// não esquecer aspas " " , ao definir a string de return.
+
+function testSize(num) {
+  if(num < 5) {
+    return "Tiny"
+  } else if (num < 10) {
+    return "Small"
+  } else if (num < 15) {
+    return "Medium"
+  } else if (num < 20){
+    return "Large"
+
+  } else {
+    return "Huge";
+  }
+}
+
+testSize(7);
+
+
+*) Golf Code
+In the game of golf, each hole has a par, meaning, the average number of strokes a golfer is expected to make in order to sink the ball in the hole to complete the play. Depending on how far above or below par your strokes are, there is a different nickname.
+
+Your function will be passed par and strokes arguments. Return the correct string according to this table which lists the strokes in order of priority; top (highest) to bottom (lowest):
+
+Strokes	Return
+1	"Hole-in-one!"
+<= par - 2	"Eagle"
+par - 1	"Birdie"
+par	"Par"
+par + 1	"Bogey"
+par + 2	"Double Bogey"
+>= par + 3	"Go Home!"
+
+ex) par and strokes will always be numeric and positive. We have added an array of all the names for your convenience.
+
+r)function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+// Change these values to test
+golfScore(5, 4);
+
+r2)var names = [
+  "Hole-in-one!",
+  "Eagle",
+  "Birdie",
+  "Par",
+  "Bogey",
+  "Double Bogey",
+  "Go Home!"
+];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
+  } else {
+    return names[6];
+  }
+  // Only change code above this line
+}
+
+r3)function golfScore(par, strokes) {
+  return strokes == 1
+    ? names[0]
+    : strokes <= par - 2
+    ? names[1]
+    : strokes == par - 1
+    ? names[2]
+    : strokes == par
+    ? names[3]
+    : strokes == par + 1
+    ? names[4]
+    : strokes == par + 2
+    ? names[5]
+    : strokes >= par + 3
+    ? names[6]
+    : "Change Me";
+}
+
+// Change these values to test
+golfScore(5, 4);
+
+
+
+
+*/
