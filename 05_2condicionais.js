9d4fa61db9622561110b5a8775276ef29fb03c53
cadumega <72901445+cadumega@users.noreply.github.com>
2021-04-19 08:00:09 -0300

    objects

--- 05_2condicionais.js
+++ 05_2condicionais.js
@@ -23,7 +23,6 @@ ex) Write a switch statement which tests val and sets answer for the following c
 R)
 function caseInSwitch(val) {
   var answer = "";
-  // Only change code below this line
   switch (val) {
     case 1:
       return "alpha";
@@ -38,17 +37,13 @@ function caseInSwitch(val) {
       return "delta";
       break;
   }
-
-  // Only change code above this line
   return answer;
 }
-
 // Change this value to test
 caseInSwitch(1);
 
 r2)function caseInSwitch(val) {
 var answer = “”;
-// Only change code below this line
 switch (val){
 case 1:
 answer = “alpha”;
@@ -63,8 +58,6 @@ case 4:
 answer = “delta”;
 break;
 }
-
-// Only change code above this line
 return answer;
 }
 
@@ -116,6 +109,162 @@ function switchOfStuff(val) {
   return answer;
 }
 
+*)Multiple Identical Options in Switch Statements
+If the break statement is omitted from a switch statement's case, the following case statement(s) are executed until a break is encountered. If you have multiple inputs with the same output, you can represent them in a switch statement like this:
 
+var result = "";
+switch(val) {
+  case 1:
+  case 2:
+  case 3:
+    result = "1, 2, or 3";
+    break;
+  case 4:
+    result = "4 alone";
+}
+Cases for 1, 2, and 3 will all produce the same result.
+
+
+ex) 
+Write a switch statement to set answer for the following ranges:
+1-3 - Low
+4-6 - Mid
+7-9 - High
+
+Note: You will need to have a case statement for each number in the range.
+
+r)function sequentialSizes(val) {
+  var answer = "";
+    switch(val) {
+      case 1:
+      case 2:
+      case 3:
+        return "Low";
+        break;
+
+      case 4:
+      case 5:
+      case 6:
+        return "Mid";
+        break;
+
+      case 7:
+      case 8:
+      case 9:
+        return "High";
+        break;
+    }
+
+  return answer;
+}
+
+sequentialSizes(1);
+
+r2)function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+  }
+  // Only change code above this line
+  return answer;
+}
+// Change this value to test
+sequentialSizes(1);
+
+*)Replacing If Else Chains with Switch
+If you have many options to choose from, a switch statement can be easier to write than many chained if/else if statements. The following:
+
+if (val === 1) {
+  answer = "a";
+} else if (val === 2) {
+  answer = "b";
+} else {
+  answer = "c";
+}
+can be replaced with:
+
+switch(val) {
+  case 1:
+    answer = "a";
+    break;
+  case 2:
+    answer = "b";
+    break;
+  default:
+    answer = "c";
+}
+
+
+ex) Change the chained if/else if statements into a switch statement.
+//   if (val === "bob") {
+//     answer = "Marley";
+//   } else if (val === 42) {
+//     answer = "The Answer";
+//   } else if (val === 1) {
+//     answer = "There is no #1";
+//   } else if (val === 99) {
+//     answer = "Missed me by this much!";
+//   } else if (val === 7) {
+//     answer = "Ate Nine";
+//   }
+
+r)
+switch (val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42:
+    answer = "The Answer";
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = "Ate Nine";
+    break;
+}
+
+r2)function chainToSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+  }
+  // Only change code above this line
+  return answer;
+}
+// Change this value to test
+chainToSwitch(7);
 
 */
---
+++ 06booleans.js
@@ -0,0 +1,153 @@
+/* 
+*) Returning Boolean Values from Functions
+You may recall from Comparison with the Equality Operator that all comparison operators return a boolean true or false value.
+
+Sometimes people use an if/else statement to do a comparison, like this:
+
+function isEqual(a,b) {
+  if (a === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+But there's a better way to do this. Since === returns true or false, we can return the result of the comparison:
+
+function isEqual(a,b) {
+  return a === b;
+}
+
+ex) Fix the function isLess to remove the if/else statements.
+
+// ex)function isLess(a, b) {
+//   if (a < b) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// isLess(10, 15);
+
+r)function isLess(a, b) {
+  return a <= b;
+}
+
+isLess(10, 15);
+
+*)Return Early Pattern for Functions
+When a return statement is reached, the execution of the current function stops and control returns to the calling location.
+
+Example
+
+function myFun() {
+  console.log("Hello");
+  return "World";
+  console.log("byebye")
+}
+myFun();
+The above will display the string Hello in the console, and return the string World. The string byebye will never display in the console, because the function exits at the return statement.
+
+ex) Modify the function abTest so that if a or b are less than 0 the function will immediately exit with a value of undefined.
+
+Hint
+Remember that undefined is a keyword, not a string.
+
+// Setup
+function abTest(a, b) {
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+r)function abTest(a, b) {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
+*ex)Counting Cards
+In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+
+Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+Count Change	Cards
++1	2, 3, 4, 5, 6
+0	7, 8, 9
+-1	10, 'J', 'Q', 'K', 'A'
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Outputs: -3 Hold or 5 Bet
+
+Hint
+Do NOT reset count to 0 when value is 7, 8, or 9.
+Do NOT return an array.
+Do NOT include quotes (single or double) in the output.
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+r)let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+r2)function cc(card) {
+  // Only change code below this line
+  var regex = /[JQKA]/;
+  if (card > 1 && card < 7) {
+    count++;
+  } else if (card === 10 || regex.test(card)) {
+    count--;
+  }
+
+  if (count > 0) return count + " Bet";
+  return count + " Hold";
+
+  // Only change code above this line
+}
+
+
+*/
