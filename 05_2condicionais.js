ab873d875c3fab7082160cb7849cbb03cd0e9627
cadumega <72901445+cadumega@users.noreply.github.com>
2021-04-18 21:39:10 -0300

    switch_break

---
+++ 05_2condicionais.js
@@ -0,0 +1,121 @@
+/* 
+*)Selecting from Many Options with Switch Statements
+If you have many options to choose from, use a switch statement. A switch statement tests a value and can have many case statements which define various possible values. Statements are executed from the first matched case value until a break is encountered.
+
+Here is an example of a switch statement:
+
+switch(lowercaseLetter) {
+  case "a":
+    console.log("A");
+    break;
+  case "b":
+    console.log("B");
+    break;
+}
+case values are tested with strict equality (===). The break tells JavaScript to stop executing statements. If the break is omitted, the next statement will be executed.
+
+ex) Write a switch statement which tests val and sets answer for the following conditions:
+1 - alpha
+2 - beta
+3 - gamma
+4 - delta 
+
+R)
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
+
+r2)function caseInSwitch(val) {
+var answer = “”;
+// Only change code below this line
+switch (val){
+case 1:
+answer = “alpha”;
+break;
+case 2:
+answer = “beta”;
+break;
+case 3:
+answer = “gamma”;
+break;
+case 4:
+answer = “delta”;
+break;
+}
+
+// Only change code above this line
+return answer;
+}
+
+// Change this value to test
+caseInSwitch(1);
+
+*)Adding a Default Option in Switch Statements
+In a switch statement you may not be able to specify all possible values as case statements. Instead, you can add the default statement which will be executed if no matching case statements are found. Think of it like the final else statement in an if/else chain.
+
+A default statement should be the last case.
+
+switch (num) {
+  case value1:
+    statement1;
+    break;
+  case value2:
+    statement2;
+    break;
+...
+  default:
+    defaultStatement;
+    break;
+}
+
+ex) Write a switch statement to set answer for the following conditions:
+a - apple
+b - bird
+c - cat
+default - stuff
+
+r)
+function switchOfStuff(val) {
+  var answer = "";
+
+  switch (val) {
+    case "a":
+      answer = "apple";
+      break;
+    case "b":
+      answer = "bird";
+      break;
+    case "c":
+      answer = "cat";
+      break;
+    default:
+      answer = "stuff";
+  }
+
+  return answer;
+}
+
+
+
+*/
